9d66d9c0f8ec9f6edf3725060b3bb31c54d8df8e
trungduong0103 <trungduong0103@gmail.com>
2021-05-02 10:41:33 +0700

    css for section list

--- src/screens/home/components/sectioned-food-list/SectionedFoodList.tsx
+++ src/screens/home/components/sectioned-food-list/SectionedFoodList.tsx
@@ -6,9 +6,9 @@ import {
   SafeAreaView,
   SectionList,
   StyleSheet,
-  TouchableWithoutFeedback,
 } from "react-native";
-import { FoodItem } from "../../../../mock-data/MockFoodList";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { FoodItem, RECIPE_TYPE } from "../../../../mock-data/MockFoodList";
 import { SectionDataObject } from "../../Home";
 import ImageButton from "../header/components/image-button/ImageButton";
 import ChevronRight from "./assets/chevron_right.png";
@@ -19,6 +19,9 @@ interface SectionedFoodListProps {
   setSearch: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+interface SectionHeaderProps {
+  title: RECIPE_TYPE;
+}
 interface FoodItemProps {
   item: FoodItem;
 }
@@ -27,37 +30,50 @@ const SectionedFoodList: React.FunctionComponent<SectionedFoodListProps> = ({
   foodByRecipeType,
   setSearch,
 }) => {
+  const SectionHeader: React.FunctionComponent<SectionHeaderProps> = ({
+    title,
+  }) => <Text style={styles.header}>{title.toUpperCase()}</Text>;
+
   const Item: React.FunctionComponent<FoodItemProps> = ({
     item,
-  }): JSX.Element => (
-    <TouchableWithoutFeedback onPress={() => setSearch(false)}>
-      <>
-        <View style={styles.item}>
-          <View style={styles.foodImageWrapper}>
-            <Image source={{ uri: item.imageUrl }} style={styles.foodImage} />
-          </View>
-          <View style={styles.rightItemWrapper}>
-            <View style={styles.foodDesriptionWrapper}>
-              <Text style={styles.foodName}>{item.foodName}</Text>
-              <View style={styles.foodDescriptionRow}>
-                <Image source={StopWatchIcon} style={styles.stopWatch} />
-                <Text style={styles.foodDescription}>
-                  {item.prepTime} min · for {item.servingPerDish} people
-                </Text>
+  }): JSX.Element => {
+    const recipes = foodByRecipeType[0].data;
+    const videoRecipes = foodByRecipeType[1].data;
+
+    const isLast =
+      item === foodByRecipeType[0].data[recipes.length - 1] ||
+      item === foodByRecipeType[1].data[videoRecipes.length - 1];
+
+    return (
+      <TouchableOpacity onPress={() => setSearch(false)}>
+        <>
+          <View style={styles.item}>
+            <View style={styles.foodImageWrapper}>
+              <Image source={{ uri: item.imageUrl }} style={styles.foodImage} />
+            </View>
+            <View style={[styles.rightItemWrapper, !isLast && styles.hr]}>
+              <View style={styles.foodDesriptionWrapper}>
+                <Text style={styles.foodName}>{item.foodName}</Text>
+                <View style={styles.foodDescriptionRow}>
+                  <Image source={StopWatchIcon} style={styles.stopWatch} />
+                  <Text style={styles.foodDescription}>
+                    {item.prepTime} min · for {item.servingPerDish} people
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.chevronWrapper}>
+                <ImageButton
+                  source={ChevronRight}
+                  handlePress={() => {}}
+                  style={styles.chevronRight}
+                />
               </View>
             </View>
-            <View style={styles.chevronWrapper}>
-              <ImageButton
-                source={ChevronRight}
-                handlePress={() => {}}
-                style={styles.chevronRight}
-              />
-            </View>
           </View>
-        </View>
-      </>
-    </TouchableWithoutFeedback>
-  );
+        </>
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <SafeAreaView style={styles.sectionListWrapper}>
@@ -66,8 +82,10 @@ const SectionedFoodList: React.FunctionComponent<SectionedFoodListProps> = ({
         keyExtractor={(item, index) => "_" + item.id + index}
         renderItem={({ item }) => <Item item={item} />}
         renderSectionHeader={({ section: { title } }) => (
-          <Text style={styles.header}>{title}</Text>
+          <SectionHeader title={title} />
         )}
+        SectionSeparatorComponent={() => <View style={styles.hr} />}
+        stickySectionHeadersEnabled={false}
       />
     </SafeAreaView>
   );
@@ -80,25 +98,28 @@ const styles = StyleSheet.create({
   },
   item: {
     paddingTop: 10,
+    paddingBottom: 10,
     paddingLeft: 5,
     paddingRight: 5,
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
     backgroundColor: "#FFF",
   },
   foodImageWrapper: {
     flex: 2,
   },
   foodImage: {
-    width: 80,
-    height: 80,
+    width: 70,
+    height: 70,
     borderRadius: 8,
     resizeMode: "cover",
   },
   rightItemWrapper: {
     flex: 5,
-    borderBottomWidth: 0.4,
     flexDirection: "row",
+  },
+  hr: {
+    borderBottomWidth: 0.4,
   },
   foodDesriptionWrapper: {
     flexDirection: "column",
@@ -133,10 +154,9 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   header: {
-    fontSize: 16,
-  },
-  hr: {
-    borderWidth: 0.5,
+    fontSize: 20,
+    marginTop: 30,
+    marginBottom: 5,
     color: "#AFAFAF",
   },
 });
